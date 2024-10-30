0a24c524f268a6fc162674bca06e2e43043491c2
irwan alfiansyah <irwanalfiansyah@Air.local>
2024-10-30 13:01:50 +0700

    first commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,13 +7,15 @@ const About = () => {
     <div>
       <SectionTitle title="About Us" />
       <div className="about-content text-center max-w-2xl mx-auto mt-5">
-      <h2 className="text-6xl text-center mb-10 max-sm:text-3xl text-accent-content">We love our customers!</h2>
+      <h2 className="text-6xl text-center mb-10 max-sm:text-3xl text-accent-content">Sahabat Kopi!</h2>
       <p className="text-lg text-center max-sm:text-sm max-sm:px-2 text-accent-content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-        obcaecati eum est commodi, quam, ut quidem deleniti quos quod temporibus
-        dicta deserunt voluptates ab! Deleniti id repellat, labore fugiat
-        obcaecati dolorem minima fugit quasi nam velit reiciendis delectus ea
-        tempora.
+        Selamat datang di Kopi Udmaktuwo! Kami adalah penyedia kopi premium yang berkomitmen
+        untuk menghadirkan kenikmatan kopi terbaik dari biji pilihan. Didirikan dengan semangat dan cinta terhadap kopi,
+        kami mengutamakan kualitas dan keaslian dalam setiap cangkir yang kami sajikan. 
+        Biji kopi kami dipilih langsung dari petani lokal, diproses dengan standar tinggi untuk memastikan
+        kesegaran dan cita rasa yang khas. Di Kopi Udmaktuwo, kami percaya bahwa setiap tegukan kopi adalah pengalaman istimewa.
+        Mari bergabung dengan kami dalam menikmati setiap momen kopi yang autentik dan penuh rasa.
+        Terima kasih telah memilih kami sebagai teman kopi Anda!
       </p>
       <Link to="/contact" className="btn btn-wide bg-blue-600 hover:bg-blue-500 text-white mt-5">Contact Us</Link>
       </div>
