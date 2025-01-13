0943e9a75c64863416d60e28adf273dc103a2fa8
lionel1046 <lionelalfi1046@gmail.com>
2025-01-13 14:18:19 +0700

    nice try

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import typography from"@tailwindcss/typography";
+import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     themes: ["coffee", "winter"],
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: ["@tailwindcss/typography", "daisyui"],
   daisyui: {
     themes: ["coffee", "light"],
   },
